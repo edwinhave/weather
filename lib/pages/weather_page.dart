6991d18bfb5acdf3820428b14e4fc85c86620494
Edwin Jhon <bluecookies.var@gmail.com>
2023-11-30 00:52:28 +0700

    weatherApp

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:weather/models/weather_model.dart';
+import 'package:weather/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('6954a8e452661acddbe834a136c5b787');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    try {
+      String cityName = await _weatherService.getCurrentCity();
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "Loading City.."),
+            Text('${_weather?.temperature.round()}°C'),
+          ],
+        ),
+      ),
+    );
+  }
+}
